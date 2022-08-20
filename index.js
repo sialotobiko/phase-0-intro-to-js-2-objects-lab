72193b3aba526b0b16cc2226925a70133caacafe
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-20 15:06:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam",
+    streetAddress : "Beverly Hills",    
+}
+
+// beforeEach(function () {
+//     for (const key in employee) {
+//       delete employee[key];
+//     }
+
+//     employee.name = 'Sam';
+//   });
+
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+  }
